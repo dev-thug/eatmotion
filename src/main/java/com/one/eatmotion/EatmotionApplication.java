453b50b8f5977443634d5941ac572d2db9465503
HJ <de0978@gmail.com>
2021-12-03 14:01:03 +0900

    Initial commit

---
+++ src/main/java/com/one/eatmotion/EatmotionApplication.java
@@ -0,0 +1,13 @@
+package com.one.eatmotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatmotionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatmotionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/one/eatmotion/EatmotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.eatmotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatmotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
