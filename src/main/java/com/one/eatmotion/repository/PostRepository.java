4f8dbcef07e145ad5effdcd03fa4f74ac396f1b3
HJ <de0978@gmail.com>
2021-12-10 16:39:26 +0900

    add post 예제

--- src/main/java/com/one/eatmotion/repository/PostRepository.java
+++ src/main/java/com/one/eatmotion/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.one.eatmotion.repository;
 
+import com.one.eatmotion.entity.Board;
 import com.one.eatmotion.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface PostRepository extends JpaRepository<Post, Long> {}
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findAllByBoard(Board board, Pageable pageable);
+}
