52fa2376b185e55f92c3684668efe1f853b09793
HJ <de0978@gmail.com>
2021-12-18 14:24:56 +0900

    update 예약 수정중

--- src/main/java/com/one/eatmotion/controller/ReserveController.java
+++ src/main/java/com/one/eatmotion/controller/ReserveController.java
@@ -3,94 +3,107 @@ package com.one.eatmotion.controller;
 import com.one.eatmotion.dto.ReserveDTO;
 import com.one.eatmotion.entity.Reserve;
 import com.one.eatmotion.service.ReserveService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"4. 맛집 리뷰"})
 @RestController
 @RequiredArgsConstructor
 public class ReserveController {
 
-  private final ReserveService reserveService;
+    private final ReserveService reserveService;
 
-  /** 매장 상세 페이지에서 시작한다고 가정 */
-  @ApiImplicitParams({
-    @ApiImplicitParam(
-        name = "authToken",
-        value = "허가된 유요한 토큰",
-        required = true,
-        dataType = "String",
-        paramType = "header")
-  })
-  @PostMapping("/reserve/{shopId}")
-  public Reserve makeReserve(@PathVariable Long shopId, @RequestBody ReserveDTO reserve) {
-    return reserveService.makeReserve(reserve, shopId);
-  }
+    /**
+     * 매장 상세 페이지에서 시작한다고 가정
+     */
+    @ApiOperation(value = "예약 하기", notes = "회원이 음식점 예약을 한다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "authToken",
+                    value = "허가된 유요한 토큰",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
+    @PostMapping("/reserve/{shopId}")
+    public Reserve makeReserve(@PathVariable Long shopId, @RequestBody ReserveDTO reserve) {
+        return reserveService.makeReserve(reserve, shopId);
+    }
 
-  /** 예약 리스트 부분 */
-  @GetMapping("/reserve/{userId}")
-  @ApiImplicitParams({
-    @ApiImplicitParam(
-        name = "authToken",
-        value = "허가된 유요한 토큰",
-        required = true,
-        dataType = "String",
-        paramType = "header")
-  })
-  public List<Reserve> listReserve(@PathVariable Long userId) {
-    System.out.println("try listReserve/{userId}");
-    return reserveService.getListByUserId(userId);
-  }
+    /**
+     * 예약 리스트 부분
+     */
+    @GetMapping("/reserves")
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "authToken",
+                    value = "허가된 유요한 토큰",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
+    @ApiOperation(value = "예약 조회", notes = "내가 예약한 음식")
+    public List<Reserve> listReserve() {
+        System.out.println("try listReserve/{userId}");
+        return reserveService.findAllByUser();
+    }
 
-  /** 예약 상세정보 확인하는 부분 */
-  @ApiImplicitParams({
-    @ApiImplicitParam(
-        name = "authToken",
-        value = "허가된 유요한 토큰",
-        required = true,
-        dataType = "String",
-        paramType = "header")
-  })
-  @GetMapping("/reserve/detail/{id}")
-  public Reserve listDetailReserve(@PathVariable Long id) {
-    System.out.println("try listReserve/{reserveId}");
-    return reserveService.getDetailListByReserveId(id);
-  }
+    /**
+     * 예약 상세정보 확인하는 부분
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "authToken",
+                    value = "허가된 유요한 토큰",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
+    @GetMapping("/reserve/{id}")
+    public Reserve listDetailReserve(@PathVariable Long id) {
+        System.out.println("try listReserve/{reserveId}");
+        return reserveService.getDetailListByReserveId(id);
+    }
 
-  /**
-   * 예약 변경하는 부분
-   *
-   * <p>reserveDate랑 reserveNumberOfPeople만 바꿀 수 있음 (임시)
-   */
-  @ApiImplicitParams({
-    @ApiImplicitParam(
-        name = "authToken",
-        value = "허가된 유요한 토큰",
-        required = true,
-        dataType = "String",
-        paramType = "header")
-  })
-  @PutMapping("/reserve/{id}")
-  public Reserve editReserve(@PathVariable Long id, @RequestBody ReserveDTO reserve) {
+    /**
+     * 예약 변경하는 부분
+     *
+     * <p>reserveDate랑 reserveNumberOfPeople만 바꿀 수 있음 (임시)
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "authToken",
+                    value = "허가된 유요한 토큰",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
+    @PutMapping("/reserve/{id}")
+    public Reserve editReserve(@PathVariable Long id, @RequestBody ReserveDTO reserve) {
 
-    return reserveService.updateReserve(id, reserve);
-  }
+        return reserveService.updateReserve(id, reserve);
+    }
 
-  /** 예약 취소하는 부분 */
-  @ApiImplicitParams({
-    @ApiImplicitParam(
-        name = "authToken",
-        value = "허가된 유요한 토큰",
-        required = true,
-        dataType = "String",
-        paramType = "header")
-  })
-  @DeleteMapping("/reserve/{reserveId}")
-  public void deleteReserve(@PathVariable Long reserveId, Reserve reserve) {
-    System.out.println("try deleteReserve");
-    reserveService.deleteReserve(reserveId);
-  }
+    /**
+     * 예약 취소하는 부분
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "authToken",
+                    value = "허가된 유요한 토큰",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
+    @DeleteMapping("/reserve/{reserveId}")
+    public void deleteReserve(@PathVariable Long reserveId) {
+        System.out.println("try deleteReserve");
+        reserveService.deleteReserve(reserveId);
+    }
 }
--- src/main/java/com/one/eatmotion/repository/ReserveRepository.java
+++ src/main/java/com/one/eatmotion/repository/ReserveRepository.java
@@ -1,11 +1,14 @@
 package com.one.eatmotion.repository;
 
 import com.one.eatmotion.entity.Reserve;
+import com.one.eatmotion.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ReserveRepository extends JpaRepository<Reserve, Long> {
+
+  List<Reserve> findAllByUser(User user);
 
   List<Reserve> getListByUserId(Long userId);
 
